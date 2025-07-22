cce19d9f3ce41643333671922c030b1aa5faaa57
tylerfoo04 <tyfoon04@gmail.com>
2025-07-22 13:54:17 -0400

    reverted

--- astdx-kaitun.lua
+++ astdx-kaitun.lua
@@ -18,13 +18,6 @@ getgenv().AutoSummonConfig = {
 loadstring(game:HttpGet("https://raw.githubusercontent.com/jfirnbrtp43/astdx-kaitun/main/astdx-kaitun.lua"))()
 ]]--
 
--- 🌐 SERVICES
-local HttpService = game:GetService("HttpService")
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local GetFunction = ReplicatedStorage:WaitForChild("Remotes"):WaitForChild("GetFunction")
-local summonDisplay = ReplicatedStorage:WaitForChild("Mods"):WaitForChild("SummonDisplay")
-local banner1 = summonDisplay:FindFirstChild("StandardSummon")
-local banner2 = summonDisplay:FindFirstChild("StandardSummon2")
 
 -- 🧠 LOAD CONFIG
 local config = getgenv().AutoSummonConfig or {}
@@ -114,7 +107,13 @@ end
 
 autoClaimQuests()
 
-
+-- 🌐 SERVICES
+local HttpService = game:GetService("HttpService")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local GetFunction = ReplicatedStorage:WaitForChild("Remotes"):WaitForChild("GetFunction")
+local summonDisplay = ReplicatedStorage:WaitForChild("Mods"):WaitForChild("SummonDisplay")
+local banner1 = summonDisplay:FindFirstChild("StandardSummon")
+local banner2 = summonDisplay:FindFirstChild("StandardSummon2")
 
 -- 🎁 REDEEM CODES
 for _, code in ipairs(redeemCodes) do
