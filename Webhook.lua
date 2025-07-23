0989390e30b0d6db3910029bfa10030be97c19a5
tylerfoo04 <tyfoon04@gmail.com>
2025-07-23 10:51:01 -0400

    fixes to webhook

--- Webhook.lua
+++ Webhook.lua
@@ -22,7 +22,7 @@ function Webhook.sendEmbedWebhook(title, description, color)
             ["footer"] = {
                 ["text"] = "**" .. username .. "**"
             },
-            ["timestamp"] = DateTime.now():ToIsoDate()
+            ["timestamp"] = DateTime.now():ToIso8601()  -- ✅ Fix here
         }}
     }
 
